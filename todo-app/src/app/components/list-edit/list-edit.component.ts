95520e157dacd4b8a4a91f18ac66f5cbe11ff8f9
דקל יוסף <u40907@sii.org.il>
2022-08-07 15:14:56 +0300

    add edit another list option

--- todo-app/src/app/components/list-edit/list-edit.component.ts
+++ todo-app/src/app/components/list-edit/list-edit.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
-import {map, Observable, switchAll, take} from "rxjs";
+import { map, Observable, Subscription, switchAll } from "rxjs";
 import { TodoList } from "../../models/todoList";
 import { StateService } from "../../services/state.service";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Color } from "../../models/color";
 import { ColorsService } from "../../services/colors.service";
 import { ValidatorsService } from "../../services/validators.service";
-import {IconsService} from "../../services/icons.service";
+import { IconsService } from "../../services/icons.service";
 
 @Component({
   selector: 'app-list-details-edit',
   templateUrl: './list-edit.component.html',
   styleUrls: ['./list-edit.component.css']
 })
-export class ListEditComponent implements OnInit {
+export class ListEditComponent implements OnInit, OnDestroy {
 
   listId!: number;
-  list$!: Observable<TodoList>;
+  lists$!: Observable<TodoList[]>;
   colors!: Color[];
   icons!: string[];
+  subscription!: Subscription;
 
   group = new FormGroup({
     caption: new FormControl('', [Validators.required]),
@@ -35,7 +36,7 @@ export class ListEditComponent implements OnInit {
               private colorsService: ColorsService,
               private iconService: IconsService,
               private validatorsService: ValidatorsService,
-              private route: ActivatedRoute,
+              private activatedRoute: ActivatedRoute,
               private router: Router) {
     this.colors = this.colorsService.getColors();
     this.icons = this.iconService.getIcons();
@@ -43,14 +44,14 @@ export class ListEditComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const index$ = this.route.params.pipe(
+    const index$ = this.activatedRoute.params.pipe(
       map(prm => Number(prm['id'])));
 
-    this.list$ = index$.pipe(
+    const list$ = index$.pipe(
       map(index => this.stateService.getListById(index)),
       switchAll());
 
-    this.list$.pipe(take(1)).subscribe(
+    this.subscription = list$.pipe().subscribe(
       next => {
         if (next !== undefined) {
           this.listId = next.id;
@@ -65,6 +66,11 @@ export class ListEditComponent implements OnInit {
       }
     );
 
+    this.lists$ = this.stateService.getAllLists();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   control(name: string): FormControl<any> {
@@ -72,7 +78,7 @@ export class ListEditComponent implements OnInit {
   }
 
   async saveList(): Promise<void> {
-    let color =  this.colorsService.getColorByCode(this.control("color").value);
+    let color = this.colorsService.getColorByCode(this.control("color").value);
     const list: TodoList = {
       id: this.listId,
       caption: this.control("caption").value,
@@ -88,4 +94,7 @@ export class ListEditComponent implements OnInit {
     await this.router.navigate(['lists']);
   }
 
+  async editAnotherList(selectedList: string): Promise<void> {
+    await this.router.navigate(['lists',  Number(selectedList), 'edit']);
+  }
 }
